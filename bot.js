6ff089ae1e88cb0739f93697d9fd8b3b49f6781f
杨乐达 <yangleda@outlook.com>
2023-01-26 19:10:11 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,29 +9,49 @@ const FACTION_ID = "41066";
 const FETCH_CHAIN_INTERVAL = 5000;  // 5 seconds
 
 let channelId = "";
-let isReportingChain = true;
+let isReportingChain = false;
 
 const bot = new eris.Client(BOT_TOKEN);
 
-bot.on('ready', () => {
-	console.log('Connected and ready.');
+bot.on("ready", () => {
+	console.log("Connected and ready.");
 });
 
-bot.on('messageCreate', async (msg) => {
+bot.on("messageCreate", async (msg) => {
 	const botWasMentioned = msg.mentions.find(
 		mentionedUser => mentionedUser.id === bot.user.id,
 	);
 	if (botWasMentioned) {
-		try {
-			await msg.channel.createMessage('Present');
-		} catch (err) {
-			console.warn('Failed to respond to mention.');
-			console.warn(err);
+		if (msg.content.includes("start")) {
+			channelId = msg.channel.id;
+			isReportingChain = true;
+			console.log("Mentioned start in channel " + msg.channel.id);
+			try {
+				await msg.channel.createMessage("Started.");
+			} catch (err) {
+				console.warn(err);
+			}
+		} else if (msg.content.includes("stop")) {
+			channelId = "";
+			isReportingChain = false;
+			console.log("Mentioned stop in channel " + msg.channel.id);
+			try {
+				await msg.channel.createMessage("Stopped.");
+			} catch (err) {
+				console.warn(err);
+			}
+		} else {
+			console.log("Mentioned in channel " + msg.channel.id);
+			try {
+				await msg.channel.createMessage("Hello World!");
+			} catch (err) {
+				console.warn(err);
+			}
 		}
 	}
 });
 
-bot.on('error', err => {
+bot.on("error", err => {
 	console.warn(err);
 });
 
@@ -47,17 +67,35 @@ async function fetchChain() {
 	try {
 		const res = await fetch(`https://api.torn.com/faction/${FACTION_ID}?selections=chain&key=${TORN_API_KEY}`);
 		const json = await res.json();
-		console.log("fetchChain result " + json);
+		console.log("fetchChain result " + json.stringify());
 		handleChain(json);
 	} catch (err) {
 		console.warn(err);
 	}
 }
 
-function handleChain(json) {
-  if (!isReportingChain) {
-	  console.log("handleChain isReportingChain is false");
+async function handleChain(json) {
+	if (!isReportingChain) {
+		console.log("handleChain isReportingChain is false");
+		return;
+	}
+	if (!channelId) {
+		console.warn("handleChain Empty channel ID");
+		return;
+	}
+	if (!json) {
+		console.warn("handleChain Empty json");
+		return;
+	}
+	let channel = bot.getChannel(channelId);
+	if (!channel) {
+		console.warn("handleChain Failed to getChannel");
 		return;
 	}
 
+	try {
+		await channel.createMessage(json.stringify());
+	} catch (err) {
+		console.warn(err);
+	}
 }
