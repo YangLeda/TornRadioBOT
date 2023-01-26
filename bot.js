4a866905add52fd16fc17d1cc642a8674d01f288
杨乐达 <yangleda@outlook.com>
2023-01-26 19:07:25 +0800

    Add fetching chain

--- bot.js
+++ bot.js
@@ -1,34 +1,63 @@
-require('dotenv').config();
+import * as dotenv from "dotenv";
+dotenv.config()
+import fetch from 'node-fetch';
+import * as eris from 'eris';
+
 const BOT_TOKEN = process.env.BOT_TOKEN;
-const eris = require('eris');
+const TORN_API_KEY = process.env.TORN_API_KEY;
+const FACTION_ID = "41066";
+const FETCH_CHAIN_INTERVAL = 5000;  // 5 seconds
+
+let channelId = "";
+let isReportingChain = true;
 
 const bot = new eris.Client(BOT_TOKEN);
 
 bot.on('ready', () => {
-   console.log('Connected and ready.');
+	console.log('Connected and ready.');
 });
 
 bot.on('messageCreate', async (msg) => {
-   const botWasMentioned = msg.mentions.find(
-       mentionedUser => mentionedUser.id === bot.user.id,
-   );
-
-   if (botWasMentioned) {
-       try {
-           await msg.channel.createMessage('Present');
-       } catch (err) {
-           // There are various reasons why sending a message may fail.
-           // The API might time out or choke and return a 5xx status,
-           // or the bot may not have permission to send the
-           // message (403 status).
-           console.warn('Failed to respond to mention.');
-           console.warn(err);
-       }
-   }
+	const botWasMentioned = msg.mentions.find(
+		mentionedUser => mentionedUser.id === bot.user.id,
+	);
+	if (botWasMentioned) {
+		try {
+			await msg.channel.createMessage('Present');
+		} catch (err) {
+			console.warn('Failed to respond to mention.');
+			console.warn(err);
+		}
+	}
 });
 
 bot.on('error', err => {
-   console.warn(err);
+	console.warn(err);
 });
 
 bot.connect();
+
+
+fetchChain();
+setInterval(async () => {
+	fetchChain();
+}, FETCH_CHAIN_INTERVAL);
+
+async function fetchChain() {
+	try {
+		const res = await fetch(`https://api.torn.com/faction/${FACTION_ID}?selections=chain&key=${TORN_API_KEY}`);
+		const json = await res.json();
+		console.log("fetchChain result " + json);
+		handleChain(json);
+	} catch (err) {
+		console.warn(err);
+	}
+}
+
+function handleChain(json) {
+  if (!isReportingChain) {
+	  console.log("handleChain isReportingChain is false");
+		return;
+	}
+
+}
