e67a22abc44f18c18e90a5d6a873b27e1ffb0447
杨乐达 <yangleda@outlook.com>
2023-01-26 21:16:45 +0800

    Add warnings

--- bot.js
+++ bot.js
@@ -7,9 +7,18 @@ const BOT_TOKEN = process.env.BOT_TOKEN;
 const TORN_API_KEY = process.env.TORN_API_KEY;
 const FACTION_ID = "41066";
 const FETCH_CHAIN_INTERVAL = 5000;  // 5 seconds
+const MIN_REPORTING_MAX_CHAIN = 10;
 
 let channelId = "";
 let isReportingChain = false;
+
+let lastMaxChain = 0;
+let tenHitsWarned = false;
+let fiveHitsWarned = false;
+
+let lastCurrentChain = 0;
+let twoMinutesWarned = false;
+let oneMinuteWarned = false;
 
 const bot = new eris.Client(BOT_TOKEN);
 
@@ -25,6 +34,7 @@ bot.on("messageCreate", async (msg) => {
 		if (msg.content.includes("start")) {
 			channelId = msg.channel.id;
 			isReportingChain = true;
+			clearAllMemory();
 			console.log("STARTED Mentioned start in channel " + msg.channel.id);
 			try {
 				await msg.channel.createMessage("Chain reporting started in this channel.");
@@ -55,8 +65,9 @@ bot.on("messageCreate", async (msg) => {
 				let timeoutSeconds = timeout - timeoutMinutes * 60;
 				let timeoutMinutesPadded = timeoutMinutes.toString().length < 2 ? "0" + timeoutMinutes.toString() : timeoutMinutes.toString();
 				let timeoutSecondsPadded = timeoutSeconds.toString().length < 2 ? "0" + timeoutSeconds.toString() : timeoutSeconds.toString();
+				let chainStr = "Chain: " + current + "/" + max + "  Timeout: " + timeoutMinutesPadded + ":" + timeoutSecondsPadded;
 				try {
-					await msg.channel.createMessage("Chain: " + current + "/" + max + "  Timeout: " + timeoutMinutesPadded + ":" + timeoutSecondsPadded + (isReportingChain ? "\nChain reporting is on." : "\nChain reporting is off."));
+					await msg.channel.createMessage(chainStr + (isReportingChain ? "\nChain reporting is on." : "\nChain reporting is off."));
 				} catch (err) {
 					console.warn(err);
 				}
@@ -73,6 +84,15 @@ bot.on("error", err => {
 
 bot.connect();
 
+function clearAllMemory() {
+	lastMaxChain = 0;
+	tenHitsWarned = false;
+	fiveHitsWarned = false;
+
+	lastCurrentChain = 0;
+	twoMinutesWarned = false;
+	oneMinuteWarned = false;
+}
 
 fetchChain();
 setInterval(async () => {
@@ -119,10 +139,54 @@ async function handleChain(json) {
 	let timeoutSeconds = timeout - timeoutMinutes * 60;
 	let timeoutMinutesPadded = timeoutMinutes.toString().length < 2 ? "0" + timeoutMinutes.toString() : timeoutMinutes.toString();
 	let timeoutSecondsPadded = timeoutSeconds.toString().length < 2 ? "0" + timeoutSeconds.toString() : timeoutSeconds.toString();
+	let chainStr = "Chain: " + current + "/" + max + "  Timeout: " + timeoutMinutesPadded + ":" + timeoutSecondsPadded;
+	let messageStr = "";
 
-	try {
-		await channel.createMessage("Chain: " + current + "/" + max + "  Timeout: " + timeoutMinutesPadded + ":" + timeoutSecondsPadded);
-	} catch (err) {
-		console.warn(err);
+	if (lastCurrentChain != current) {
+		twoMinutesWarned = false;
+		oneMinuteWarned = false;
+		lastCurrentChain = current;
+	}
+	if (lastMaxChain != max) {
+		tenHitsWarned = false;
+		fiveHitsWarned = false;
+		twoMinutesWarned = false;
+		oneMinuteWarned = false;
+		lastMaxChain = max;
+	}
+
+	if (max >= MIN_REPORTING_MAX_CHAIN && timeout <= 60) { // Warn 1 minute till timeout
+		if (!oneMinuteWarned) {
+			oneMinuteWarned = true;
+			messageStr = chainStr + "\n" + "Chain is timing out in 1 minute! Make another hit to keep the chain alive!";
+		}
+	} else if (max >= MIN_REPORTING_MAX_CHAIN && timeout <= 120) { // Warn 2 minutes till timeout
+		if (!twoMinutesWarned) {
+			twoMinutesWarned = true;
+			messageStr = chainStr + "\n" + "Chain is timing out in 2 minutes! Make another hit to keep the chain alive!";
+		}
+	}
+
+	if (max >= MIN_REPORTING_MAX_CHAIN && max - current <= 5) { // Warn 5 hits till bonus
+		if (!fiveHitsWarned) {
+			fiveHitsWarned = true;
+			messageStr = chainStr + "\n" + (max - current) + "hits till bonus hit! Make sure the bonus hit is on enemy faction.";
+		}
+	} else if (max >= MIN_REPORTING_MAX_CHAIN && max - current <= 10) {  // Warn 10 hits till bonus
+		if (!tenHitsWarned) {
+			tenHitsWarned = true;
+			messageStr = chainStr + "\n" + (max - current) + "hits till bonus hit! Make sure the bonus hit is on enemy faction.";
+		}
+	}
+
+	if (messageStr != "") {
+		try {
+			console.log("handleChain Sending warning: " + messageStr);
+			await channel.createMessage(messageStr);
+		} catch (err) {
+			console.warn(err);
+		}
+	} else {
+		console.log("handleChain " + chainStr);
 	}
 }
